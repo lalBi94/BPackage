371c2ed273076fa512f411bf1a3872ba06a9e6bd
pro.boooooo <pro.boudjemline@gmail.com>
2022-10-25 21:24:04 +0200

    $

--- src/BDatabase.java
+++ src/BDatabase.java
@@ -8,23 +8,36 @@
 import java.sql.*;
 import java.sql.Connection;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
 import org.mariadb.jdbc.*;
 
-// Maitre
+import javax.swing.*;
+
+/**
+ * <p>Methodes pour les interaction avec une base de donnees</p>
+ * @author <a href="https://github.com/lalBi94">Bilal Boudjemline</a>
+ * */
 
 public class BDatabase {
-    public String db_host;
-    public String db_name;
-    public String db_user;
-    protected String db_password;
+    private final String db_host;
+    private final String db_name;
+    private final String db_user;
+    private final String db_password;
     protected Connection sharedObject;
-    protected int current_user;
+    private boolean status;
 
-    public BDatabase() {
-        this.db_host = "jdbc:mariadb://" + "HOST";
-        this.db_name = "DB";
-        this.db_user = "USER";
-        this.db_password = "PASSWORD";
+    /**
+     * @param host Adresse de l'hote (jdbc:mariadb://+HOST)
+     * @param db Nom de la base de donnees
+     * @param user Utilisateur
+     * @param password Mot de passe
+     * */
+    public BDatabase(String host, String db, String user, String password) {
+        this.db_host = "jdbc:mariadb://" + host;
+        this.db_name = db;
+        this.db_user = user;
+        this.db_password = password;
 
         try {
             Class.forName("org.mariadb.jdbc.Driver");
@@ -35,12 +48,34 @@ public class BDatabase {
         try {
             this.sharedObject = DriverManager.getConnection(this.db_host + this.db_name, this.db_user, this.db_password);
             System.out.println("Success Connected.");
+            this.status = true;
         } catch (SQLException e) {
-            System.out.println("Error with SQL connexion.\n" + e);
-            System.exit(-1);
+            System.out.println("Erreur de liaison avec la base de donnees.");
         }
     }
 
+    /**
+     * Creer un tableau en parametre de fonction pour les requetes qui necessite des colonnes
+     *
+     * @param val Selecteur de colonnes
+     * @return Retourne le tableau de colonnes a traite
+     * */
+    public String[] inlineArray(String... val) {
+        String[] tmp = new String[val.length];
+
+        for(int i = 0; i <= val.length-1; i++) {
+            tmp[i] = val[i];
+        }
+
+        return tmp;
+    }
+
+    /**
+     * Recuperer les informations d'un requete de type SELECT.
+     *
+     * @param request Le SELECT a faire (il y aura plus d'argument prochainement)
+     * @return Les resultats engendre par la requete
+     */
     public ArrayList<String> fetchAll(String request) {
         try {
             ArrayList<String> toReturn = new ArrayList<String>();
@@ -53,11 +88,58 @@ public class BDatabase {
             System.out.println("Succes: " + request);
             return toReturn;
         } catch(SQLException e) {
-            System.out.println("Error with the statement : " + e);
+            System.out.println("Erreur de la requete : " + e);
             return null;
         }
     }
 
+    /**
+     * Insert une ligne dans une table.
+     *
+     * @param table La table
+     * @param col   Tableau qui contient les colonnes d'affectation
+     * @param value Valeur des colonnes
+     * @return Retourne true si ca a reussit et false dans le cas inverse
+     */
+    public boolean insertRow(String table, String[] col, String[] value) {
+        StringBuilder collumns = new StringBuilder();
+        StringBuilder values = new StringBuilder();
+        Iterator<String> iteCol = Arrays.stream(col).iterator();
+        Iterator<String> iteVal = Arrays.stream(value).iterator();
+
+        collumns.append("(");
+        while(iteCol.hasNext()) {
+            collumns.append(iteCol.next()).append(", ");
+        }
+        collumns.setLength(collumns.length()-2);
+        collumns.append(")");
+
+        values.append("(");
+        while(iteVal.hasNext()) {
+            values.append("\"").append(iteVal.next()).append("\"").append(", ");
+        }
+        values.setLength(values.length()-2);
+        values.append(")");
+
+        String request = "INSERT INTO " + table + collumns + " VALUES" + values + ";";
+
+        System.out.println(request);
+
+        try {
+            this.sharedObject.prepareStatement(request).executeQuery();
+            System.out.println("Succes: " + request);
+            return true;
+        } catch(SQLException e) {
+            return false;
+        }
+    }
+
+    /**
+     * Faire des requetes de type UPDATE SET (il y aura plus d'argument prochainement).
+     *
+     * @param request the request
+     * @return the boolean
+     */
     public boolean updateRow(String request) {
         try {
             this.sharedObject.prepareStatement(request).executeQuery();
@@ -68,20 +150,44 @@ public class BDatabase {
         }
     }
 
+    /**
+     * Recuperer l'objet Connection pour pouvoir utiliser ses methodes.
+     *
+     * @return L'objet Connection
+     */
     public Connection getSharedObject() {
         return this.sharedObject;
     }
 
+    /**
+     * Recuperer l'utilisateur courant.
+     *
+     * @return L'utilisateur
+     */
     public String getUser() {
         return this.db_user;
     }
 
+    /**
+     * Recuperer l'hote courant.
+     *
+     * @return L'hote
+     */
     public String getHost() {
         return this.db_host;
     }
 
+    /**
+     * Recuperer le nom de la base de donnees.
+     *
+     * @return Le nom de la base de donnees
+     */
     public String getDatabaseName() {
         return this.db_name;
+    }
+
+    public boolean getStatus() {
+        return this.status;
     }
 
     @Override
--- src/BFrame.java
+++ src/BFrame.java
@@ -1,72 +1,111 @@
-/* [BFrame]
-*  Desc: To create Operationnal JFrame fast :)
-*  GitHub: https://github.com/lalBi94
-*  Created by: Bilal Boudjemline
-*  28/09/2022 at 20:35
-* */
-
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 import javax.swing.ImageIcon;
 import java.awt.GridBagLayout;
+import java.awt.event.WindowEvent;
+
+/**
+ * <p>Pour creer une fenetre rapidement avec les methodes simplifier</p>
+ * @author <a href="https://github.com/lalBi94">Bilal Boudjemline</a>
+ * */
 
 public class BFrame extends JFrame {
-    public String title;
-    public int location_x = 1;
-    public int location_y = 1;
-    public int width = 500;
-    public int height = 500;
-    public boolean isOpen = false;
-    public int onClose = 3;
+    private final String title;
+    private int location_x = 1;
+    private int location_y = 1;
+    private int width = 500;
+    private int height = 500;
+    private int killProcess = 3;
+    private boolean isOpen = false;
 
-    public BFrame(String titlee, int oC) {
-        this.title = titlee;
-        this.onClose = oC;
+    /**
+     * Avec le titre et mode de fermeture.
+     *
+     * @param title Le titre de la fenetre
+     * @param oC    Mode de fermeture de la fenetre
+     */
+    public BFrame(String title, int oC) {
+        this.title = title;
+        this.killProcess = oC;
         initBFrame();
     }
 
+    /**
+     * Avec le titre, la taille (en L et l) et mode de fermeture.
+     *
+     * @param titlee Le titre de la fenetre
+     * @param size_x La longueur de la fenetre
+     * @param size_y La largeur y de la fenetre
+     * @param oC     Mode de fermeture de la fenetre
+     */
     public BFrame(String titlee, int size_x, int size_y, int oC) {
         this.title = titlee;
         this.width = size_x;
         this.height = size_y;
-        this.onClose = oC;
+        this.killProcess = oC;
         initBFrame();
     }
 
-    public BFrame(String titlee, int loca_x, int loca_y, int size_x, int size_y, int oC) {
-        this.title = titlee;
+    /**
+     * Avec le titre, la locasation (en x et y), la taille (en L et l) et mode de fermeture.
+     *
+     * @param title  Le titre de la fenetre
+     * @param loca_x La localisation en x de la fenetre
+     * @param loca_y La localisation en y de la fenetre
+     * @param size_x La longueur de la fenetre
+     * @param size_y La largeur de la fenetre
+     * @param oC     Mode de fermeture de la fenetre
+     */
+    public BFrame(String title, int loca_x, int loca_y, int size_x, int size_y, int oC) {
+        this.title = title;
         this.location_x = loca_x;
         this.location_y = loca_y;
         this.width = size_x;
         this.height = size_y;
-        this.onClose = oC;
+        this.killProcess = oC;
         initBFrame();
     }
 
+    /**
+     * Initialiser la fenetre
+     */
     protected void initBFrame() {
-        ImageIcon icon = new ImageIcon("src/assets/img/logo.png");
+        ImageIcon icon = new ImageIcon("assets/logo.png");
         this.setTitle(this.title);
         this.setLocation(this.location_x, this.location_y);
         this.setSize(this.width, this.height);
         this.setLayout(new GridBagLayout());
         this.setIconImage(icon.getImage());
-        this.setDefaultCloseOperation(this.onClose);
+        this.setDefaultCloseOperation(this.killProcess);
     }
 
+    /**
+     * Ouvrir la fenetre
+     */
     public void openBFrame() {
         this.isOpen = true;
         this.setVisible(true);
     }
 
+    /**
+     * Fermer la fenetre
+     */
     public void closeBFrame() {
         this.isOpen = false;
         this.setVisible(false);
+        this.dispatchEvent(new WindowEvent(this, WindowEvent.WINDOW_CLOSING));
     }
 
+    /**
+     * Rafraichir la fenetre
+     */
     public void refreshBFrame() {
         SwingUtilities.updateComponentTreeUI(this);
     }
 
+    /**
+     * Statut de visibilite de la fenetre
+     */
     public boolean isVisible() {
         return this.isOpen;
     }
